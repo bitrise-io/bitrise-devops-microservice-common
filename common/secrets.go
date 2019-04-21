eea1f5013a0dc2368a46d262d1cc240a80231850
Viktor Benei <viktor.benei@gmail.com>
2019-04-21 09:48:50 +0200

    Read secret file from the SECRETS_CONFIG_DIR_PATH and from its subdirs (non recursive)

--- common/secrets.go
+++ common/secrets.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/bitrise-io/go-utils/envutil"
 	"github.com/bitrise-io/go-utils/fileutil"
+	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/pkg/errors"
 	"github.com/viktorbenei/bitrise-devops-microservice-common/common/logger"
 	"go.uber.org/zap"
@@ -18,28 +19,71 @@ type Config map[string]string
 // If subPath is specified it'll search for the secret file in that sub directory.
 // The secret file's name has to match the secretKey, and should
 // have no extension.
-func LoadSecret(secretKey, subPath string) (string, error) {
+func LoadSecret(secretKey string) (string, error) {
+	// First check if it's set as an env var.
+	// Continue if not.
 	if envVar := os.Getenv(secretKey); len(envVar) > 0 {
 		return envVar, nil
 	}
 
-	// check in files
+	// Check if it's a file (with filename==secretKey) in the secrets config dir.
+	// Continue if not.
 	secretConfigDirPath := envutil.GetenvWithDefault("SECRETS_CONFIG_DIR_PATH", "/var/secret")
-	if len(subPath) > 0 {
-		secretConfigDirPath = filepath.Join(secretConfigDirPath, subPath)
-	}
 	secretConfigFilePath := filepath.Join(secretConfigDirPath, secretKey)
-	secretValue, err := fileutil.ReadStringFromFile(secretConfigFilePath)
-	if err != nil {
-		logger.L.Error("Failed to open secret config file",
+	if exist, err := pathutil.IsPathExists(secretConfigFilePath); err != nil {
+		logger.L.Error("Failed to check if secret file exist at path",
 			zap.String("path", secretConfigFilePath),
+			zap.Error(err),
 		)
-	} else if len(secretValue) < 1 {
-		logger.L.Error("Secret file found but was empty",
-			zap.String("path", secretConfigFilePath),
+	} else if exist {
+		secretValue, err := fileutil.ReadStringFromFile(secretConfigFilePath)
+		if err != nil {
+			logger.L.Error("Failed to open secret config file",
+				zap.String("path", secretConfigFilePath),
+				zap.Error(err),
+			)
+		} else if len(secretValue) < 1 {
+			logger.L.Error("Secret file found but was empty",
+				zap.String("path", secretConfigFilePath),
+			)
+		} else {
+			return secretValue, nil
+		}
+	}
+
+	// Check if it's a file (with filename==secretKey) is a SUBDIRECTORY of secrets config dir.
+	// Continue if not.
+	filepathPattern := filepath.Join(secretConfigDirPath, "*", secretKey)
+	files, err := filepath.Glob(filepathPattern)
+	if err != nil {
+		logger.L.Error("Failed to list files for pattern",
+			zap.String("path", filepathPattern),
+			zap.Error(err),
+		)
+	}
+	if len(files) < 1 {
+		logger.L.Error("No file found for pattern",
+			zap.String("path", filepathPattern),
+		)
+	} else if len(files) > 1 {
+		logger.L.Error("More than 1 file found for pattern",
+			zap.String("path", filepathPattern),
 		)
 	} else {
-		return secretValue, nil
+		secretConfigFilePath := files[0]
+		secretValue, err := fileutil.ReadStringFromFile(secretConfigFilePath)
+		if err != nil {
+			logger.L.Error("Failed to open secret config file",
+				zap.String("path", secretConfigFilePath),
+				zap.Error(err),
+			)
+		} else if len(secretValue) < 1 {
+			logger.L.Error("Secret file found but was empty",
+				zap.String("path", secretConfigFilePath),
+			)
+		} else {
+			return secretValue, nil
+		}
 	}
 
 	// not found
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/viktorbenei/bitrise-devops-microservice-common
 require (
 	github.com/bitrise-io/go-utils v0.0.0-20181112170818-2a09aab8380d
 	github.com/pkg/errors v0.8.1
+	github.com/stretchr/testify v1.3.0 // indirect
 	go.uber.org/atomic v1.3.2 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.9.1
